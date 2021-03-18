f2a68ccc458f5c975a37b3d1789fcb5e16920c7a
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-18 17:04:07 +0900

    Added global CSS styles and module styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,8 @@
 import React from "react"
 import Navbar from "./Navbar"
+//To use the global CSS. we must use the component in which every page has. In this case it is the layout component
+import "../styles/global.css"
+
 // in the layout component, the contents are passed through the props as children
 export default function Layout({ children }) {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import { header, btn } from "../styles/home.module.css" // <-- changes in Gatsby v3 - CSS needs to be destructured to be used in the tags
+// The styles module is a CSS styling to scope one component of the site.
+//Only this component will have styling from this module
+//The CSS styling scopes the specific HTML tags in this component
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
           <h2>Design</h2>
-          <h3> Develop & Deploy</h3>
-          Hello world! Testing Testing Testing Testing Testing Testing Testing
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Tokyo.</p>
+          <Link className={btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
