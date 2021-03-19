3f83d205c95807c6d5858a064da8ccc2fe00930a
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-19 14:23:18 +0900

    Added projects list to projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,48 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
-import { portfolio } from "../../styles/projects.module.css"
+import { portfolio, projects } from "../../styles/projects.module.css"
 
-export default function ProjectIndex() {
+//graphql queries become the props of the component page
+
+export default function ProjectIndex({ data }) {
+  console.log(data)
+
+  const projectArray = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={projects}>
+          {projectArray.map(project => (
+            <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// export page query
+
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
