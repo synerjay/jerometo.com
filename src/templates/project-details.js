362943c1b520cb09c0657ecd1ed88ad3519c4a6a
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-19 19:24:18 +0900

    Finished...for now

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -3,17 +3,26 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Img from "gatsby-image"
-import { details, featured, html } from "../styles/project-details.module.css"
+import {
+  details,
+  featured,
+  content,
+} from "../styles/project-details.module.css"
 import { graphql } from "gatsby"
 
 const ProjectDetails = ({ data }) => {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
       <div className={details}>
-        <h2>title</h2>
-        <h3>stack</h3>
-        <div className={featured}>{/* <Img fluid={} /> */}</div>
-        {/* <div className={html} dangerouslySetInnerHTML={}/> */}
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
+        <div className={featured}>
+          <Img fluid={featuredImg.childImageSharp.fluid} />{" "}
+        </div>
+        <div className={content} dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
   )
@@ -31,7 +40,7 @@ export const query = graphql`
         featuredImg {
           childImageSharp {
             fluid {
-              src
+              ...GatsbyImageSharpFluid
             }
           }
         }
