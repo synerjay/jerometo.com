a2e28a85d7027707bfbc49dda0a38754af3162fe
Jerome Tolentino <tolent.jay@gmail.com>
2021-08-20 10:04:08 +0900

    changed the logo color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,21 @@ export default function Navbar() {
   return (
     <nav id="main">
       <Link className="flex items-center text-sm font-bold" to="/">
-        JEROME <h2 className="font-logo text-yellow-900 text-6xl">JT</h2>{" "}
-        TOLENTINO
+        JEROME <h2 className="font-logo text-black text-6xl">JT</h2> TOLENTINO
       </Link>
       <div className="links">
         <Link className="font-bold relative w-max two" to="/">
-          <span className="uppercase">/About</span>{" "}
+          <span className="uppercase">About</span>{" "}
           <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-1 bg-yellow-400"></span>
           <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-1 bg-yellow-400"></span>
         </Link>
         <Link className="font-bold relative w-max two" to="/work">
-          <span className="uppercase">/Work</span>
+          <span className="uppercase">Work</span>
           <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-1 bg-yellow-400"></span>
           <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-1 bg-yellow-400"></span>
         </Link>
         <Link className="font-bold relative w-max two" to="/contact">
-          <span className="uppercase">/Contact</span>{" "}
+          <span className="uppercase">Contact</span>{" "}
           <span class="absolute -bottom-1 left-1/2 w-0 transition-all h-1 bg-yellow-400"></span>
           <span class="absolute -bottom-1 right-1/2 w-0 transition-all h-1 bg-yellow-400"></span>
         </Link>
